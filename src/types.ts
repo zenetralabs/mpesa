ad5bf426f8ceae42fb9aa2c7afa714c7f812266f
Abel Misiocha <wells.mail.13@gmail.com>
2024-12-14 12:47:51 +0300

    feat(core): Update types

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+export type MpesaResponseType = { data: any; error: any }
+
+export type ResponseTypeType = 'Completed' | 'Cancelled'
+
+export type B2BCommandsType =
+    | 'BusinessPayBill'
+    | 'BusinessBuyGoods'
+    | 'DisburseFundsToBusiness'
+    | 'BusinessToBusinessTransfer'
+    | 'MerchantToMerchantTransfer'
+
+export type B2CCommandsType = 'BusinessPayment' | 'SalaryPayment' | 'PromotionPayment'
+
+export type MpesaConfigType = {
+    env: 'sandbox' | 'live'
+    type: number
+    shortcode: number
+    store: number | null
+    key: string
+    secret: string
+    username: string
+    password: string
+    passkey: string
+}
+
+export type MpesaSTKResponseType = {
+    MerchantRequestID: string | null
+    CheckoutRequestID: string | null
+    ResponseCode: number | null
+    ResponseDescription: string | null
+    CustomerMessage: string | null
+    errorCode?: number | null
+    errorMessage?: string | null
+}
